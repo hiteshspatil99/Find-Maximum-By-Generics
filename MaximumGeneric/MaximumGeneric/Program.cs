072726028f7604f5fc5bb789854d3b4702f55ee0
Hitesh <hiteshspatil99@gmail.com>
2021-11-20 00:44:53 +0530

    [Hitesh] add : Finding Maximum Integers Using Generic

---
+++ MaximumGeneric/MaximumGeneric/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaximumGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the Program that want to be executed : \n 1. Find Maximum Integer \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First number, Second number, Third number : ");
+                        int first = Convert.ToInt32(Console.ReadLine());
+                        int second = Convert.ToInt32(Console.ReadLine());
+                        int third = Convert.ToInt32(Console.ReadLine());
+                        int result = MaximumSolution.FindMax(first, second, third);
+                        Console.WriteLine("Maximum Number is : " + result);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
