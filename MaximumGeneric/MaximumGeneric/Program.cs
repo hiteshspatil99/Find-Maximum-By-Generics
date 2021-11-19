849dd4311899c61c88374071b4b4cb57c68478dd
Hitesh <hiteshspatil99@gmail.com>
2021-11-20 00:50:50 +0530

    [Hitesh] add : Finding Maximum Float Using Generic

--- MaximumGeneric/MaximumGeneric/Program.cs
+++ MaximumGeneric/MaximumGeneric/Program.cs
@@ -9,7 +9,7 @@ namespace MaximumGeneric
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program that want to be executed : \n 1. Find Maximum Integer \n 2.Exit");
+                Console.WriteLine("Enter the Program that want to be executed : \n 1. Find Maximum Integer \n 2. Find Maximum Float \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -22,6 +22,14 @@ namespace MaximumGeneric
                         Console.WriteLine("Maximum Number is : " + result);
                         break;
                     case 2:
+                        Console.WriteLine("Enter First number, Second number, Third number to check : ");
+                        double firstNum = Convert.ToDouble(Console.ReadLine());
+                        double secondNum = Convert.ToDouble(Console.ReadLine());
+                        double thirdNum = Convert.ToDouble(Console.ReadLine());
+                        double res = MaximumSolution.FindMax(firstNum, secondNum, thirdNum);
+                        Console.WriteLine("The Maximum Number is : " + res);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
